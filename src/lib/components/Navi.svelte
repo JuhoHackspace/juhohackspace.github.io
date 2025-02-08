05a14918ca8078d3930d4f207588877b3e2aad40
Juho Hietala <juhohietala90@gmail.com>
2025-02-09 00:32:58 +0200

    Slide animation logic and nav bar implemented

--- src/lib/components/Navi.svelte
+++ src/lib/components/Navi.svelte
@@ -1,13 +1,18 @@
 <script>
+    import { page } from '$app/stores';
+    import { derived } from 'svelte/store';
     import "$lib/styles/navStyles.css";
-</script>
+  
+    // Create a derived store to get the current path
+    const currentPath = derived(page, $page => $page.url.pathname);
+  </script>
 
 <div class="header-nav-container">
     <nav class="header-nav">
         <ul>
-            <li><a href="/about">Who am I?</a></li>
-            <li><a href="/blog">My Work</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <li><a href="/" class={$currentPath === '/' ? 'active' : ''}>Who am I?</a></li>
+            <li><a href="/myWork" class={$currentPath === '/myWork' ? 'active' : ''}>My Work</a></li>
+            <li><a href="/contact" class={$currentPath === '/myWork' ? 'active' : ''}>Contact</a></li>
         </ul>
     </nav>
 </div>
